b07e305980dc64953890250500daeab65943f082
tekmark1 <tekmark1@gmail.com>
2016-03-04 21:01:48 -0600

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -446,5 +446,6 @@ def disconnect():
 if __name__ == '__main__':
     app.secret_key = 'super_secret_key'
     app.debug = True
-    app.run(host='127.0.0.1', port=8080)
+    app.run()
+    #host='127.0.0.1', port=8080
     #host='0.0.0.0', port=8000)
